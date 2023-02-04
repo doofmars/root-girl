6805cbc33026cda996046fa4caa3d861fac2cf7b
doofmars <jan-preuss@gmx.de>
2023-02-04 23:06:37 +0100

    Animate the flag

--- src/objects/EndFlag.gd
+++ src/objects/EndFlag.gd
@@ -3,3 +3,6 @@ extends Area2D
 func _on_EndFlag_body_entered(body):
 	if body.get_meta('type') == 'girl':
 		var parent = get_parent().touch_flag()
+
+func _ready():
+	self.get_node("AnimationPlayer").play("Flag")
