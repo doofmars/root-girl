05acbde590455a4934da5b7d667c50e3308dbccd
Felix <site-github@penth.in>
2023-02-04 15:23:41 +0100

    Liana Anchoring

--- src/character/Liana.gd
+++ src/character/Liana.gd
@@ -4,29 +4,32 @@ var liana_active = false
 var liana_destination = Vector2.ZERO
 var liana_has_hit = false
 
-var SCALE_DIFFERENCE = 4.135
+export var VECTOR_SCALE = 400
 
-export var liana_speed = 1
+export var liana_speed = 2
 
 func _process(delta):
 	if liana_active and liana_destination != Vector2.ZERO and !liana_has_hit:
 		$LianaSprite.look_at(liana_destination)
 		$LianaSprite.scale.x += liana_speed * delta
-		$CollisionShape2D.look_at(liana_destination)
-		$CollisionShape2D.scale.x += liana_speed * delta * SCALE_DIFFERENCE
+		
+		var velo = (liana_destination - $LianaCollisionShape.global_position).normalized() * VECTOR_SCALE * liana_speed * delta
+		$LianaCollisionShape.position += velo
+	if liana_has_hit:
+		$LianaSprite.look_at(liana_destination)
+		$LianaSprite.scale.x = (self.global_position - liana_destination).length()/411
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("liana"):
 		var space_state = get_world_2d().direct_space_state
 
 		# use global coordinates, not local to node
-		var result = space_state.intersect_ray(global_position, get_viewport().get_mouse_position(), [self], 1, true, true)
+		var result = space_state.intersect_ray(global_position, get_viewport().get_mouse_position(), [self, get_parent()], 2, true, true)
 		if result:
 			liana_active = true
 			liana_destination = result.position
 
-func _on_Liana_area_entered(area:Area2D):
-	print("_on_Liana_area_entered", area)
-	# liana_has_hit = true
-	# liana_active = true
-	# liana_destination = Vector2.ZERO
+
+func _on_Liana_area_shape_entered(area_rid:RID, area:Area2D, area_shape_index:int, local_shape_index:int):
+	liana_active = false
+	liana_has_hit = true
