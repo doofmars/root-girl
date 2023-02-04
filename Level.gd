9d3c3f49754a24b4893beb952d53b39704c90cb1
doofmars <jan-preuss@gmx.de>
2023-02-04 17:36:45 +0100

    Camera is fixed to the Player and blocked by the corners

--- Level.gd
+++ Level.gd
@@ -3,10 +3,25 @@ extends Node
 var starting_position
 var girl
 
+const LIMIT_LEFT = 0
+const LIMIT_TOP = 0
+const LIMIT_BOTTOM = 360
+
 func _ready():
 	girl = get_node("Girl")
 	starting_position = girl.position
 	
+	var map = self.get_node("TileMap")
+	var rect = map.get_used_rect()
+	
+	for child in get_children():
+		if child is Girl:
+			var camera = child.get_node("Camera")
+			camera.limit_left = LIMIT_LEFT
+			camera.limit_top = LIMIT_TOP
+			camera.limit_right = rect.end.x * map.cell_quadrant_size
+			camera.limit_bottom = LIMIT_BOTTOM
+
 func _process(delta):
 	if Input.is_action_pressed("action_restart"):
 		notify_player_death()
