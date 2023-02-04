4cc4ebcb164b22e8635ce5608f5dcb03466edcd3
AplusKminus <apluskminus@gmail.com>
2023-02-04 20:02:43 +0100

    Add game timer

--- src/game.gd
+++ src/game.gd
@@ -1,8 +1,10 @@
 extends Node
 
 signal show_menu
+signal game_time
 
 var current_level
+var start_time_offset
 
 const levels = [
 	"Plain", 
@@ -13,6 +15,8 @@ const levels = [
 func _on_Menu_run_game():
 	if current_level == null:
 		start_level(levels[0])
+		start_time_offset = OS.get_ticks_msec()
+		get_node("GameTimer").start()
 		
 func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
@@ -25,6 +29,7 @@ func level_finished(level_name):
 	else:
 		current_level.queue_free()
 		current_level = null
+		get_node("GameTimer").stop()
 		emit_signal("show_menu", "Restart")
 	
 func start_level(level_name):
@@ -33,3 +38,17 @@ func start_level(level_name):
 		current_level.queue_free()
 	current_level = level_scene.instance()
 	add_child(current_level)
+	get_node("ScoreHUD").show()
+
+
+func _on_GameTimer_timeout():
+	var millis_elapsed = OS.get_ticks_msec() - start_time_offset
+	var tenths = (millis_elapsed / 100) % 10
+	var seconds = (millis_elapsed / 1_000) % 60
+	var minutes = (millis_elapsed / 60_000) % 60
+	var hours = millis_elapsed / 3_600_000
+
+	var result: String = "%02d:%02d:%02d.%d" % [hours, minutes, seconds, tenths]
+	while result.begins_with("00:"):
+		result = result.substr(3)
+	emit_signal("game_time", result)
