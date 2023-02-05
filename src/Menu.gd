779e36159ce2fd05198ed99399aa7087c24369a4
Felix <site-github@penth.in>
2023-02-05 10:10:44 +0100

    Add Music

--- src/Menu.gd
+++ src/Menu.gd
@@ -14,6 +14,7 @@ func _on_StartButton_pressed():
 func go_to_game():
 	get_tree().paused = false
 	hide()
+	switchMusic(false)
 	emit_signal("run_game")
 
 func _on_QuitButton_pressed():
@@ -22,4 +23,17 @@ func _on_QuitButton_pressed():
 func _on_Game_show_menu(text):
 	get_tree().paused = true
 	$StartButton.text = text
+	switchMusic(true)
 	show()
+
+func switchMusic(paused):
+	var levelMusicPlayer = get_node("../LevelMusicPlayer")
+	var menuMusicPlayer = get_node("../MenuMusicPlayer")
+	print(levelMusicPlayer)
+
+	if paused:
+		levelMusicPlayer.stop()
+		menuMusicPlayer.play(levelMusicPlayer.get_playback_position())
+	else:
+		menuMusicPlayer.stop()
+		levelMusicPlayer.play(menuMusicPlayer.get_playback_position())
